a83e896940b5b7fec35928a12309f53351fba6bb
AlexWu18 <alejandrowu189@gmail.com>
2022-08-30 17:46:18 -0400

    Add project files.

---
+++ Crud/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud
+{
+    public partial class Form1 : Form
+    {
+        INTEC_AGU_OCT22Entities db = new INTEC_AGU_OCT22Entities();
+        List<string> Msg = new List<string>();
+        string PeopleId = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
